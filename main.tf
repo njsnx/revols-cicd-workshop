1c16f0a1f58e5e0370066aca25463dd0ec1d2423
Neil Stewart <neil.x.stewart@pwc.com>
2022-03-31 13:21:54 +0100

    Fixing Workflow

--- main.tf
+++ main.tf
@@ -8,10 +8,3 @@ terraform {
 
 
 
-
-resource "aws_ssm_parameter" "a-parameter" {
-  count = 100
-  name  = "/cicd/workshop/my-parameter-${count.index}"
-  type  = "String"
-  value = "Some value"
-}
